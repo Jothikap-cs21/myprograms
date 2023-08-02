8b3ef27fd9048883aad456da143fc340d08783e2
jothikap-cs21 <jothikap.cs21@kascsathy.ac.in>
2023-08-02 06:14:56 +0000

    second commit

--- c/matrix-multiplication.c
+++ c/matrix-multiplication.c
@@ -8,42 +8,33 @@
     printf("enter the number of column=");    
     scanf("%d",&c);    
     printf("enter the first matrix element=\n");    
-    for(i=0;i<r;i++)    
-    {    
-    for(j=0;j<c;j++)    
-    {    
-    scanf("%d",&a[i][j]);    
-    }    
+    for(i=0;i<r;i++) {    
+	 for(j=0;j<c;j++) {    
+		  scanf("%d",&a[i][j]);    
+    	 }    
     }    
     printf("enter the second matrix element=\n");    
-    for(i=0;i<r;i++)    
-    {    
-    for(j=0;j<c;j++)    
-    {    
-    scanf("%d",&b[i][j]);    
-    }    
+    for(i=0;i<r;i++) {    
+	    for(j=0;j<c;j++)  {    
+		    scanf("%d",&b[i][j]);    
+    	    }    
     }    
         
     printf("multiply of the matrix=\n");    
-    for(i=0;i<r;i++)    
-    {    
-    for(j=0;j<c;j++)    
-    {    
-    mul[i][j]=0;    
-    for(k=0;k<c;k++)    
-    {    
-    mul[i][j]+=a[i][k]*b[k][j];    
-    }    
-    }    
+    for(i=0;i<r;i++) {    
+    	for(j=0;j<c;j++){    
+    		mul[i][j]=0;    
+    		for(k=0;k<c;k++) {    
+    			mul[i][j]+=a[i][k]*b[k][j];    
+    		}    
+    	}    
     }    
     //for printing result    
-    for(i=0;i<r;i++)    
-    {    
-    for(j=0;j<c;j++)    
-    {    
-    printf("%d\t",mul[i][j]);    
-    }    
-    printf("\n");    
+    for(i=0;i<r;i++) {    
+   	 for(j=0;j<c;j++) {    
+    		printf("%d\t",mul[i][j]);    
+    	 }    
+    	 printf("\n");    
     }    
     return 0;  
     }  
